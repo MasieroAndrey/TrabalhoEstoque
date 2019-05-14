b1bda4c6c004b265a282f538d4482f563fe01da6
AntonioVianaA <aaviana2000@gmail.com>
2019-05-13 21:22:43 -0300

    View Login

---
+++ Estoque/View/FazerLogin.cs
@@ -0,0 +1,33 @@
+﻿using Estoque.Model;
+using Estoque.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estoque.View
+{
+    class FazerLogin
+    {
+        public static void Renderiazar()
+        {
+            Usuario u = new Usuario();
+
+            Console.WriteLine("--Fazer LOGiN--");
+            Console.WriteLine("Digite o nome de usuario:");
+            u.Login = Console.ReadLine();
+            // No  site da para pedir verificação dupla de senha
+            Console.WriteLine("Digite a senha do usuario:");
+            u.Password = Console.ReadLine();
+            if (ValidaLogin.ValidarLogin(u) != null)
+            {
+                Console.WriteLine("\nLogin bem sucedido");
+            }
+            else
+            {
+                Console.WriteLine("\nUsuario ou senha incorretos ou não cadastrados");
+            }
+        }
+    }
+}
