9ba9b85162de241e7f717835fcf592d262ddd450
AntonioVianaA <aaviana2000@gmail.com>
2019-05-13 21:25:18 -0300

    View CadastrarProduto

--- Estoque/View/CadastrarProduto.cs
+++ Estoque/View/CadastrarProduto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Estoque.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,24 @@ namespace Estoque.View
 {
     class CadastrarProduto
     {
+        public static void Renderizar()
+        {
+            Produto p = new Produto();
+            Console.WriteLine("  -- CADASTRAR PRODUTO --\n");
+            //Console.WriteLine("Digite o nome do produto:");
+            //p.Nome = Console.ReadLine();
+            Console.WriteLine("Digite o preço do produto:");
+            p.Preco = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a quantidade do produto:");
+            p.Quantidade = Convert.ToInt32(Console.ReadLine());
+            if (ProdutoDAO.CadastrarProduto(p))
+            {
+                Console.WriteLine("\nProduto cadastrado com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("\nEsse produto já existe!");
+            }
+        }
     }
 }
