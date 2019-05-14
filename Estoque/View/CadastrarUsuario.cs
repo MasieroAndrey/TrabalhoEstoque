4af43a6bbfe7c0c23631f2032dd9bb3aee7482ff
AntonioVianaA <aaviana2000@gmail.com>
2019-05-13 21:26:17 -0300

    View Cadastrar Usuario

--- Estoque/View/CadastrarUsuario.cs
+++ Estoque/View/CadastrarUsuario.cs
@@ -1,4 +1,7 @@
-﻿     using System;
+﻿using Estoque.DAL;
+using Estoque.Model;
+using Estoque.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,37 @@ namespace Estoque.View
 {
     class CadastrarUsuario
     {
+        public static void Renderizar()
+        {
+            Usuario u = new Usuario();
+            Console.WriteLine("  -- CADASTRAR CLIENTE --\n");
+            Console.WriteLine("Digite seu nome completo:");
+            u.Nome = Console.ReadLine();
+            Console.WriteLine("Digite o CPF do usuario:");
+            u.Cpf = Console.ReadLine();
+            // API para validar cpf
+            if (Validacao.ValidarCpf(u.Cpf))
+            {
+                Console.WriteLine("Digite seu nome de usuario:");
+                u.Login = Console.ReadLine();
+                Console.WriteLine("Digite a senha:");
+                u.Password = Console.ReadLine();
+                Console.WriteLine("informe seu saldo:");
+                u.SaldoConta = Convert.ToDouble(Console.ReadLine());
+                u.DataCadastro = DateTime.Now;
+                if (UsuarioDAO.CadastrarUsuario(u))
+                {
+                    Console.WriteLine("\nUsuario cadastrado com sucesso!");
+                }
+                else
+                {
+                    Console.WriteLine("\nEsse usuario já existe!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("CPF inválido!");
+            }
+        }
     }
 }
