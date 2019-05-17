c5716a2a766bf4d868251231b38b80e8785954a0
Andrey Masiero <andrey.masiero@hotmail.com>
2019-05-16 21:47:35 -0300

    Classes DAL criadas

---
+++ Estoque/DAL/EnderecoDAO.cs
@@ -0,0 +1,17 @@
+﻿using Estoque.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estoque.DAL
+{
+    class EnderecoDAO
+    {
+        private static Context ctx = SingletonContext.GetInstance();
+
+
+
+    }
+}
